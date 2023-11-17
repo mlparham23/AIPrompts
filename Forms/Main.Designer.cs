73ca1186c2bf1d8eb8e8b5a3bd68fb401816c12d
mlparham23 <mlparham@hotmail.com>
2023-11-17 13:33:42 -0600

    Created GUI Interface

--- Forms/Main.Designer.cs
+++ Forms/Main.Designer.cs
@@ -28,12 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            grpbxImages = new GroupBox();
+            grpbxChat = new GroupBox();
+            btnImagesAddPrompt = new Button();
+            btnImagesDeletePrompt = new Button();
+            btnImagesSearchPrompt = new Button();
+            btnChatSearchPrompt = new Button();
+            btnChatDeletePrompt = new Button();
+            btnChatAddPrompt = new Button();
+            btnExit = new Button();
+            mnustrp = new MenuStrip();
+            mnustrpFile = new ToolStripMenuItem();
+            mnustrpFileExit = new ToolStripMenuItem();
+            mnustrpImages = new ToolStripMenuItem();
+            mnustrpImagesAddPrompt = new ToolStripMenuItem();
+            mnustrpImagesDeletePrompt = new ToolStripMenuItem();
+            mnustrpImagesSearchPrompt = new ToolStripMenuItem();
+            mnustrpChat = new ToolStripMenuItem();
+            mnustrpChatAddPrompt = new ToolStripMenuItem();
+            mnustrpChatDeletePrompt = new ToolStripMenuItem();
+            mnustrpChatSearchPrompt = new ToolStripMenuItem();
+            grpbxImages.SuspendLayout();
+            grpbxChat.SuspendLayout();
+            mnustrp.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grpbxImages
+            // 
+            grpbxImages.Controls.Add(btnImagesSearchPrompt);
+            grpbxImages.Controls.Add(btnImagesDeletePrompt);
+            grpbxImages.Controls.Add(btnImagesAddPrompt);
+            grpbxImages.Location = new Point(12, 30);
+            grpbxImages.Name = "grpbxImages";
+            grpbxImages.Size = new Size(189, 159);
+            grpbxImages.TabIndex = 0;
+            grpbxImages.TabStop = false;
+            grpbxImages.Text = "Images";
+            // 
+            // grpbxChat
+            // 
+            grpbxChat.Controls.Add(btnChatSearchPrompt);
+            grpbxChat.Controls.Add(btnChatAddPrompt);
+            grpbxChat.Controls.Add(btnChatDeletePrompt);
+            grpbxChat.Location = new Point(207, 30);
+            grpbxChat.Name = "grpbxChat";
+            grpbxChat.Size = new Size(190, 159);
+            grpbxChat.TabIndex = 1;
+            grpbxChat.TabStop = false;
+            grpbxChat.Text = "Chat";
+            // 
+            // btnImagesAddPrompt
+            // 
+            btnImagesAddPrompt.BackColor = Color.LightBlue;
+            btnImagesAddPrompt.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnImagesAddPrompt.Location = new Point(6, 22);
+            btnImagesAddPrompt.Name = "btnImagesAddPrompt";
+            btnImagesAddPrompt.Size = new Size(174, 37);
+            btnImagesAddPrompt.TabIndex = 0;
+            btnImagesAddPrompt.Text = "Add Prompt";
+            btnImagesAddPrompt.UseVisualStyleBackColor = false;
+            // 
+            // btnImagesDeletePrompt
+            // 
+            btnImagesDeletePrompt.BackColor = Color.LightBlue;
+            btnImagesDeletePrompt.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnImagesDeletePrompt.Location = new Point(6, 65);
+            btnImagesDeletePrompt.Name = "btnImagesDeletePrompt";
+            btnImagesDeletePrompt.Size = new Size(174, 37);
+            btnImagesDeletePrompt.TabIndex = 1;
+            btnImagesDeletePrompt.Text = "Delete Prompt";
+            btnImagesDeletePrompt.UseVisualStyleBackColor = false;
+            // 
+            // btnImagesSearchPrompt
+            // 
+            btnImagesSearchPrompt.BackColor = Color.LightBlue;
+            btnImagesSearchPrompt.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnImagesSearchPrompt.Location = new Point(6, 108);
+            btnImagesSearchPrompt.Name = "btnImagesSearchPrompt";
+            btnImagesSearchPrompt.Size = new Size(174, 37);
+            btnImagesSearchPrompt.TabIndex = 2;
+            btnImagesSearchPrompt.Text = "Search Prompt";
+            btnImagesSearchPrompt.UseVisualStyleBackColor = false;
+            // 
+            // btnChatSearchPrompt
+            // 
+            btnChatSearchPrompt.BackColor = Color.LightBlue;
+            btnChatSearchPrompt.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnChatSearchPrompt.Location = new Point(6, 108);
+            btnChatSearchPrompt.Name = "btnChatSearchPrompt";
+            btnChatSearchPrompt.Size = new Size(174, 37);
+            btnChatSearchPrompt.TabIndex = 5;
+            btnChatSearchPrompt.Text = "Search Prompt";
+            btnChatSearchPrompt.UseVisualStyleBackColor = false;
+            // 
+            // btnChatDeletePrompt
+            // 
+            btnChatDeletePrompt.BackColor = Color.LightBlue;
+            btnChatDeletePrompt.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnChatDeletePrompt.Location = new Point(6, 65);
+            btnChatDeletePrompt.Name = "btnChatDeletePrompt";
+            btnChatDeletePrompt.Size = new Size(174, 37);
+            btnChatDeletePrompt.TabIndex = 4;
+            btnChatDeletePrompt.Text = "Delete Prompt";
+            btnChatDeletePrompt.UseVisualStyleBackColor = false;
+            // 
+            // btnChatAddPrompt
+            // 
+            btnChatAddPrompt.BackColor = Color.LightBlue;
+            btnChatAddPrompt.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnChatAddPrompt.Location = new Point(6, 22);
+            btnChatAddPrompt.Name = "btnChatAddPrompt";
+            btnChatAddPrompt.Size = new Size(174, 37);
+            btnChatAddPrompt.TabIndex = 3;
+            btnChatAddPrompt.Text = "Add Prompt";
+            btnChatAddPrompt.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            btnExit.BackColor = Color.LightBlue;
+            btnExit.Font = new Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point);
+            btnExit.Location = new Point(213, 195);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(174, 37);
+            btnExit.TabIndex = 6;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = false;
+            // 
+            // mnustrp
+            // 
+            mnustrp.Items.AddRange(new ToolStripItem[] { mnustrpFile, mnustrpImages, mnustrpChat });
+            mnustrp.Location = new Point(0, 0);
+            mnustrp.Name = "mnustrp";
+            mnustrp.Size = new Size(409, 24);
+            mnustrp.TabIndex = 7;
+            mnustrp.Text = "menuStrip1";
+            // 
+            // mnustrpFile
+            // 
+            mnustrpFile.DropDownItems.AddRange(new ToolStripItem[] { mnustrpFileExit });
+            mnustrpFile.Name = "mnustrpFile";
+            mnustrpFile.Size = new Size(37, 20);
+            mnustrpFile.Text = "File";
+            // 
+            // mnustrpFileExit
+            // 
+            mnustrpFileExit.Name = "mnustrpFileExit";
+            mnustrpFileExit.Size = new Size(180, 22);
+            mnustrpFileExit.Text = "Exit";
+            // 
+            // mnustrpImages
+            // 
+            mnustrpImages.DropDownItems.AddRange(new ToolStripItem[] { mnustrpImagesAddPrompt, mnustrpImagesDeletePrompt, mnustrpImagesSearchPrompt });
+            mnustrpImages.Name = "mnustrpImages";
+            mnustrpImages.Size = new Size(57, 20);
+            mnustrpImages.Text = "Images";
+            // 
+            // mnustrpImagesAddPrompt
+            // 
+            mnustrpImagesAddPrompt.Name = "mnustrpImagesAddPrompt";
+            mnustrpImagesAddPrompt.Size = new Size(180, 22);
+            mnustrpImagesAddPrompt.Text = "Add Prompt";
+            // 
+            // mnustrpImagesDeletePrompt
+            // 
+            mnustrpImagesDeletePrompt.Name = "mnustrpImagesDeletePrompt";
+            mnustrpImagesDeletePrompt.Size = new Size(180, 22);
+            mnustrpImagesDeletePrompt.Text = "Delete Prompt";
+            // 
+            // mnustrpImagesSearchPrompt
+            // 
+            mnustrpImagesSearchPrompt.Name = "mnustrpImagesSearchPrompt";
+            mnustrpImagesSearchPrompt.Size = new Size(180, 22);
+            mnustrpImagesSearchPrompt.Text = "Search Prompt";
+            // 
+            // mnustrpChat
+            // 
+            mnustrpChat.DropDownItems.AddRange(new ToolStripItem[] { mnustrpChatAddPrompt, mnustrpChatDeletePrompt, mnustrpChatSearchPrompt });
+            mnustrpChat.Name = "mnustrpChat";
+            mnustrpChat.Size = new Size(44, 20);
+            mnustrpChat.Text = "Chat";
+            // 
+            // mnustrpChatAddPrompt
+            // 
+            mnustrpChatAddPrompt.Name = "mnustrpChatAddPrompt";
+            mnustrpChatAddPrompt.Size = new Size(180, 22);
+            mnustrpChatAddPrompt.Text = "Add Prompt";
+            // 
+            // mnustrpChatDeletePrompt
+            // 
+            mnustrpChatDeletePrompt.Name = "mnustrpChatDeletePrompt";
+            mnustrpChatDeletePrompt.Size = new Size(180, 22);
+            mnustrpChatDeletePrompt.Text = "Delete Prompt";
+            // 
+            // mnustrpChatSearchPrompt
+            // 
+            mnustrpChatSearchPrompt.Name = "mnustrpChatSearchPrompt";
+            mnustrpChatSearchPrompt.Size = new Size(180, 22);
+            mnustrpChatSearchPrompt.Text = "Search Prompt";
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(409, 242);
+            Controls.Add(btnExit);
+            Controls.Add(grpbxChat);
+            Controls.Add(grpbxImages);
+            Controls.Add(mnustrp);
+            MainMenuStrip = mnustrp;
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AI Prompts";
+            grpbxImages.ResumeLayout(false);
+            grpbxChat.ResumeLayout(false);
+            mnustrp.ResumeLayout(false);
+            mnustrp.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox grpbxImages;
+        private Button btnImagesAddPrompt;
+        private GroupBox grpbxChat;
+        private Button btnImagesSearchPrompt;
+        private Button btnImagesDeletePrompt;
+        private Button btnChatSearchPrompt;
+        private Button btnChatAddPrompt;
+        private Button btnChatDeletePrompt;
+        private Button btnExit;
+        private MenuStrip mnustrp;
+        private ToolStripMenuItem mnustrpFile;
+        private ToolStripMenuItem mnustrpFileExit;
+        private ToolStripMenuItem mnustrpImages;
+        private ToolStripMenuItem mnustrpImagesAddPrompt;
+        private ToolStripMenuItem mnustrpImagesDeletePrompt;
+        private ToolStripMenuItem mnustrpImagesSearchPrompt;
+        private ToolStripMenuItem mnustrpChat;
+        private ToolStripMenuItem mnustrpChatAddPrompt;
+        private ToolStripMenuItem mnustrpChatDeletePrompt;
+        private ToolStripMenuItem mnustrpChatSearchPrompt;
     }
 }
